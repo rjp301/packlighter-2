1bd0388c94f1f5205e9d876ad4babf24c8eb4e27
Riley Paul <rileypaul96@gmail.com>
2024-07-10 22:44:08 -0600

    update seed function

--- db/seed.ts
+++ db/seed.ts
@@ -1,6 +1,10 @@
 import { generateId } from "@/api/helpers/generate-id";
 import { Category, CategoryItem, Item, List, User, db } from "astro:db";
-import { randomItemFromArray, randomNumberWithinRange } from "./seeds/utils";
+import {
+  randomItemFromArray,
+  randomLengthArray,
+  randomNumberWithinRange,
+} from "./seeds/utils";
 import { categoryNames } from "./seeds/category-names";
 import { itemNamesDescs } from "./seeds/item-names-descs";
 import { weightUnits } from "@/api/helpers/weight-units";
@@ -46,15 +50,13 @@ export default async function seed() {
         userId,
         name,
         description,
-        categories: new Array(randomNumberWithinRange(2, 7)).fill(0).map(() => {
-          const categoryItems = new Array(randomNumberWithinRange(2, 7))
-            .fill(0)
-            .map(() =>
-              initCategoryItem({
-                itemId: randomItemFromArray(items).id,
-                quantity: randomNumberWithinRange(1, 10),
-              }),
-            );
+        categories: randomLengthArray(2, 7).map(() => {
+          const categoryItems = randomLengthArray(2, 7).map(() =>
+            initCategoryItem({
+              itemId: randomItemFromArray(items).id,
+              quantity: randomNumberWithinRange(1, 10),
+            }),
+          );
           return initCategory({
             name: randomItemFromArray(categoryNames),
             items: categoryItems,
@@ -64,31 +66,4 @@ export default async function seed() {
     )
     .returning();
   console.log(`✅ Seeded ${lists.length} lists`);
-
-  const categories = await db
-    .insert(Category)
-    .values(
-      new Array(20).fill(0).map(() => ({
-        id: generateId(),
-        listId: randomItemFromArray(lists).id,
-        userId,
-        name: randomItemFromArray(categoryNames),
-      })),
-    )
-    .returning();
-  console.log(`✅ Seeded ${categories.length} categories`);
-
-  const categoryItems = await db
-    .insert(CategoryItem)
-    .values(
-      new Array(100).fill(0).map(() => ({
-        id: generateId(),
-        userId,
-        categoryId: randomItemFromArray(categories).id,
-        itemId: randomItemFromArray(items).id,
-        quantity: randomNumberWithinRange(1, 10),
-      })),
-    )
-    .returning();
-  console.log(`✅ Seeded ${categoryItems.length} categoryItems`);
 }
--- db/seeds/utils.ts
+++ db/seeds/utils.ts
@@ -5,3 +5,7 @@ export const randomItemFromArray = <T>(arr: T[]): T => {
 export const randomNumberWithinRange = (min: number, max: number): number => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
+
+export const randomLengthArray = (min: number, max: number): any[] => {
+  return new Array(randomNumberWithinRange(min, max)).fill(0);
+};
