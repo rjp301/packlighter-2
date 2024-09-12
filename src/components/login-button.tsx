ce76f2cc8f5f63f318674bd5cd2f2d0e058f2b05
Riley Paul <rileypaul96@gmail.com>
2024-09-12 16:30:07 -0600

    add guest variant to login button

--- src/components/login-button.tsx
+++ src/components/login-button.tsx
@@ -4,24 +4,30 @@ import { buttonVariants } from "./ui/button";
 
 import { FaGithub } from "react-icons/fa6";
 import { FcGoogle } from "react-icons/fc";
+import { UserRound } from "lucide-react";
 
 const authProviders: Record<
   string,
-  { name: string; url: string; icon: React.ReactNode; className?: string }
+  { string: string; url: string; icon: React.ReactNode; className?: string }
 > = {
   github: {
-    name: "GitHub",
+    string: "Continue with GitHub",
     url: "/login/github",
     icon: <FaGithub className="mr-2 h-5 w-5" />,
     className:
       "bg-gray-900 text-white hover:bg-gray-800 border border-gray-700 shadow",
   },
   google: {
-    name: "Google",
+    string: "Continue with Google",
     url: "/login/google",
     icon: <FcGoogle className="mr-2 h-5 w-5" />,
     className:
       "bg-white text-black hover:bg-gray-100 shadow border border-gray-300",
+  },
+  guest: {
+    string: "Continue as Guest",
+    url: "/login/guest",
+    icon: <UserRound className="mr-2 h-5 w-5" />,
   },
 };
 
@@ -44,7 +50,7 @@ const LoginButton: React.FC<Props> = (props) => {
       href={authProvider.url}
     >
       <span className="absolute left-6">{authProvider.icon}</span>
-      <span>Continue with {authProvider.name}</span>
+      <span>{authProvider.string}</span>
     </a>
   );
 };
