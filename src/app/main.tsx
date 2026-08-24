c814f318a0692ae16be137e0583c2c431898cf21
Riley Paul <rileypaul96@gmail.com>
2026-08-23 21:08:41 -0600

    add store provider

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -12,6 +12,8 @@ import CustomToaster from "@/app/components/ui/custom-toaster";
 import { toast } from "sonner";
 import ErrorDisplay from "@/app/components/ui/error-display";
 import Loader from "@/app/components/ui/loader";
+import { Provider } from "jotai";
+import { store } from "./store";
 
 const queryClient = new QueryClient({
   defaultOptions: { queries: { staleTime: 1000 * 60 * 5 } },
@@ -49,12 +51,14 @@ declare module "@tanstack/react-router" {
 }
 
 const App: React.FC = () => (
-  <QueryClientProvider client={queryClient}>
-    <RadixProvider>
-      <CustomToaster />
-      <RouterProvider router={router} />
-    </RadixProvider>
-  </QueryClientProvider>
+  <Provider store={store}>
+    <QueryClientProvider client={queryClient}>
+      <RadixProvider>
+        <CustomToaster />
+        <RouterProvider router={router} />
+      </RadixProvider>
+    </QueryClientProvider>
+  </Provider>
 );
 
 export default App;
